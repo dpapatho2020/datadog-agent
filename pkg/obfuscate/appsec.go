021509be5c25c1572d2a52a2e80377427162a231
Julio Guerra <julio@datadog.com>
2022-03-15 11:59:31 +0100

    backup

--- pkg/obfuscate/appsec.go
+++ pkg/obfuscate/appsec.go
@@ -7,7 +7,11 @@ package obfuscate
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 // ObfuscateAppSec obfuscates the given appsec tag value in order to remove sensitive values from the appsec security
@@ -129,4 +133,524 @@ func matchString(re *regexp.Regexp, s string) bool {
 		return false
 	}
 	return re.MatchString(s)
+}
+
+type appsecEventsObfuscator struct {
+	keyRE, valueRE *regexp.Regexp
+}
+
+var (
+	errUnexpectedType        = errors.New("unexpected appsec event type")
+	errUnexpectedEndOfString = errors.New("unexpected end of appsec event string")
+)
+
+func (o *appsecEventsObfuscator) obfuscateKeyPath(scanner *scanner, input string, i *int, output strings.Builder, keyRE *regexp.Regexp) (sensitiveKey bool, err error) {
+	if scanner.step(scanner, input[*i]) != scanBeginArray {
+		return false, errUnexpectedType
+	}
+	*i++
+	depth := 0
+	literalFrom := 0
+loop:
+	for {
+		c := input[*i]
+		op := scanner.step(scanner, c)
+		switch op {
+		case scanBeginObject, scanBeginArray:
+			depth++
+
+		case scanEndObject:
+			depth--
+
+		case scanEndArray:
+			depth--
+			if depth == 0 {
+				break loop
+			}
+
+		case scanBeginLiteral, scanContinue:
+			if depth == 0 && literalFrom == -1 {
+				literalFrom = *i
+			}
+
+		case scanArrayValue:
+			value, err := strconv.Unquote(input[literalFrom:*i])
+			if err != nil {
+				continue
+			}
+			if matchString(keyRE, value) {
+				sensitiveKey = true
+				break
+			}
+
+		case scanError:
+			return false, scanner.err
+		}
+		*i++
+	}
+	return sensitiveKey, nil
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecTriggers(scanner *scanner, input string, i int, diff *Diff) (int, error) {
+	i, err := stepTo(scanner, input, i, scanBeginArray)
+	if err != nil {
+		return i, err
+	}
+	for i += 1; i < len(input); i++ {
+		i, err = o.obfuscateAppSecTrigger(scanner, input, i, diff)
+		if err == errUnexpectedType {
+			i, err = stepUntil(scanner, input, i, scanArrayValue)
+			if err != nil {
+				return i, err
+			}
+			continue
+		}
+		if err != nil {
+			return i, err
+		}
+	}
+	return i, errUnexpectedType
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecTrigger(scanner *scanner, input string, i int, diff *Diff) (int, error) {
+	i, err := stepTo(scanner, input, i, scanBeginObject)
+	if err != nil {
+		return i, err
+	}
+	var (
+		key string
+	)
+	for i += 1; i < len(input); i++ {
+		key, i, err = scannObjectKey(scanner, input, i)
+		if err == errUnexpectedType {
+			i, err = stepUntil(scanner, input, i, scanObjectValue)
+			if err != nil {
+				return i, err
+			}
+			continue
+		}
+		if err != nil {
+			return i, err
+		}
+		if key == "rule_matches" {
+			i, err = o.obfuscateAppSecRuleMatches(scanner, input, i, diff)
+			if err == errUnexpectedType {
+				i, err = stepUntil(scanner, input, i, scanObjectValue)
+				if err != nil {
+					return i, err
+				}
+				continue
+			}
+			if err != nil {
+				return i, err
+			}
+		}
+		i, err = stepObjectValue(scanner, input, i)
+		if err != nil && err != errUnexpectedType {
+			return i, err
+		}
+	}
+	return i, errUnexpectedType
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecRuleMatches(scanner *scanner, input string, i int, diff *Diff) (int, error) {
+	i, err := stepTo(scanner, input, i, scanBeginArray)
+	if err != nil {
+		return i, err
+	}
+	for i += 1; i < len(input); i++ {
+		i, err = o.obfuscateAppSecRuleMatch(scanner, input, i, diff)
+		if err == errUnexpectedType {
+			i, err = stepUntil(scanner, input, i, scanArrayValue)
+			if err != nil {
+				return i, err
+			}
+			continue
+		}
+		if err != nil {
+			return i, err
+		}
+	}
+	return i, errUnexpectedType
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecRuleMatch(scanner *scanner, input string, i int, diff *Diff) (int, error) {
+	i, err := stepTo(scanner, input, i, scanBeginObject)
+	if err != nil {
+		return i, err
+	}
+	var (
+		key string
+	)
+	for i += 1; i < len(input); i++ {
+		key, i, err = scannObjectKey(scanner, input, i)
+		if err == errUnexpectedType {
+			i, err = stepUntil(scanner, input, i, scanObjectValue)
+			if err != nil {
+				return i, err
+			}
+			continue
+		}
+		if err != nil {
+			return i, err
+		}
+		if key == "parameters" {
+			i, err = o.obfuscateAppSecRuleParameters(scanner, input, i, diff)
+			if err == errUnexpectedType {
+				i, err = stepUntil(scanner, input, i, scanObjectValue)
+				if err != nil {
+					return i, err
+				}
+				continue
+			}
+			if err != nil {
+				return i, err
+			}
+		}
+		i, err = stepObjectValue(scanner, input, i)
+		if err != nil && err != errUnexpectedType {
+			return i, err
+		}
+	}
+	return i, errUnexpectedType
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecRuleParameters(scanner *scanner, input string, i int, diff *Diff) (int, error) {
+	i, err := stepTo(scanner, input, i, scanBeginArray)
+	if err != nil {
+		return i, err
+	}
+	for i += 1; i < len(input); i++ {
+		i, err = o.obfuscateAppSecRuleParameter(scanner, input, i, diff)
+		if err == errUnexpectedType {
+			i, err = stepUntil(scanner, input, i, scanArrayValue)
+			if err != nil {
+				return i, err
+			}
+			continue
+		}
+		if err != nil {
+			return i, err
+		}
+	}
+	return i, errUnexpectedType
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecRuleParameter(scanner *scanner, input string, i int, diff *Diff) (int, error) {
+	return i, nil
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecParameterKeyPath(scanner *scanner, input string, i int) (hasSensitiveKey bool, j int, err error) {
+	i, err = obfuscateArrayStrings(scanner, input, i, func(from, to int) {
+		// Ignore the call if we already found a sensitive key in a previous call
+		if hasSensitiveKey {
+			return
+		}
+		value := input[from : to+1]
+		value, err := unquote(value)
+		if err != nil {
+			return
+		}
+		if matchString(o.keyRE, value) {
+			hasSensitiveKey = true
+		}
+	})
+	if err != nil {
+		return false, i, err
+	}
+	return hasSensitiveKey, i, nil
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecParameterHighlight(scanner *scanner, input string, i int, diff *Diff, hasSensitiveKey bool) (int, error) {
+	i, err := obfuscateArrayStrings(scanner, input, i, func(from, to int) {
+		if hasSensitiveKey {
+			diff.Add(from, to, `"?"`)
+			return
+		}
+		value, err := unquote(input[from : to+1])
+		if err != nil {
+			return
+		}
+		if !matchString(o.valueRE, value) {
+			return
+		}
+		value = o.valueRE.ReplaceAllString(value, "?")
+		value, err = quote(value)
+		if err != nil {
+			return
+		}
+		diff.Add(from, to, value)
+	})
+	if err != nil {
+		return i, err
+	}
+	return i, nil
+}
+
+type Diff []struct {
+	from, to int
+	value    string
+}
+
+func (d *Diff) Add(from, to int, value string) {
+	*d = append(*d, struct {
+		from  int
+		to    int
+		value string
+	}{from, to, value})
+}
+
+func (d Diff) Apply(input string) string {
+	from := 0
+	var output strings.Builder
+	for _, diff := range d {
+		output.WriteString(input[from:diff.from])
+		from = diff.to + 1
+		output.WriteString(diff.value)
+	}
+	output.WriteString(input[from:])
+	return output.String()
+}
+
+func obfuscateArrayStrings(scanner *scanner, input string, i int, visit func(from, to int)) (int, error) {
+	i, err := stepTo(scanner, input, i, scanBeginArray)
+	if err != nil {
+		return i, err
+	}
+	stringFrom := -1
+	depth := 0
+	for ; i < len(input); i++ {
+		c := input[i]
+		switch scanner.step(scanner, c) {
+		case scanBeginObject, scanBeginArray:
+			depth++
+		case scanEndObject:
+			depth--
+		case scanEndArray:
+			if depth == 0 {
+				return i + 1, nil
+			}
+			depth--
+
+		case scanBeginLiteral:
+			if depth == 0 && input[i] == '"' {
+				stringFrom = i
+			}
+		case scanContinue:
+			if stringFrom != -1 && input[i] == '"' && input[i-1] != '\\' {
+				visit(stringFrom, i)
+				stringFrom = -1
+			}
+
+		case scanError:
+			return i, scanner.err
+		}
+	}
+	if stringFrom != -1 {
+		return i, errUnexpectedEndOfString
+	}
+	return i, errUnexpectedType
+}
+
+func walkObject(scanner *scanner, input string, i int, visit func(key, value string)) (int, error) {
+	i, err := stepTo(scanner, input, i, scanBeginObject)
+	if err != nil {
+		return i, err
+	}
+	keyFrom := -1
+	keyTo := -1
+	valueFrom := -1
+	depth := 0
+	for ; i < len(input); i++ {
+		switch scanner.step(scanner, input[i]) {
+		case scanBeginObject, scanBeginArray:
+			depth++
+		case scanEndArray:
+			depth--
+		case scanEndObject:
+			if depth != 0 {
+				depth--
+				continue
+			}
+			// We reached the end of the object we were scanning
+			if keyFrom != -1 && keyTo != -1 && valueFrom != -1 {
+				// Visit the last value of the objet
+				visit(input[keyFrom:keyTo], input[valueFrom:i])
+			}
+			return i + 1, nil
+
+		case scanBeginLiteral:
+			if depth != 0 || keyFrom != -1 {
+				continue
+			}
+			if input[i] == '"' {
+				keyFrom = i
+			}
+		case scanContinue:
+			if keyFrom != -1 && keyTo == -1 && input[i] == '"' && input[i-1] != '\\' {
+				keyTo = i + 1
+			}
+
+		case scanObjectKey:
+			if depth == 0 {
+				valueFrom = i + 1
+			}
+		case scanObjectValue:
+			if depth != 0 {
+				continue
+			}
+			visit(input[keyFrom:keyTo], input[valueFrom:i])
+			keyFrom = -1
+			keyTo = -1
+			valueFrom = -1
+
+		case scanError:
+			return i, scanner.err
+		}
+	}
+	if keyFrom != -1 || valueFrom != -1 {
+		return i, errUnexpectedEndOfString
+	}
+	return i, errUnexpectedType
+}
+
+func (o *appsecEventsObfuscator) obfuscateAppSecParameterValue(scanner *scanner, input string, i int, diff *Diff, hasSensitiveKey bool) (int, error) {
+	value, from, i, err := scanString(scanner, input, i)
+	if err != nil {
+		return i, err
+	}
+	if hasSensitiveKey {
+		diff.Add(from, i-1, `"?"`)
+		return i, nil
+	}
+	value, err = unquote(value)
+	if err != nil {
+		return i, err
+	}
+	if !matchString(o.valueRE, value) {
+		return i, nil
+	}
+	value = o.valueRE.ReplaceAllString(value, "?")
+	value, err = quote(value)
+	if err != nil {
+		return i, err
+	}
+	diff.Add(from, i-1, value)
+	return i, nil
+}
+
+func scannObjectKey(scanner *scanner, input string, i int) (key string, j int, err error) {
+	i, err = stepTo(scanner, input, i, scanBeginLiteral)
+	if err != nil {
+		return "", i, err
+	}
+	from := i
+	for i += 1; i < len(input); i++ {
+		switch scanner.step(scanner, input[i]) {
+		case scanError:
+			return "", i, scanner.err
+		case scanObjectKey:
+			value, err := strconv.Unquote(input[from:i])
+			if err != nil {
+				return "", i, err
+			}
+			return value, i + 1, nil
+		case scanEndObject:
+			return "", i + 1, nil
+		}
+	}
+	return "", i, errUnexpectedType
+}
+
+func stepObjectValue(scanner *scanner, input string, i int) (j int, err error) {
+	return stepUntil(scanner, input, i, scanObjectValue)
+}
+
+func stepTo(scanner *scanner, input string, i int, to int) (int, error) {
+	for ; i < len(input); i++ {
+		switch scanner.step(scanner, input[i]) {
+		default:
+			return i + 1, errUnexpectedType
+		case scanSkipSpace, scanContinue:
+			continue
+		case scanError:
+			return i + 1, scanner.err
+		case to:
+			return i + 1, nil
+		}
+	}
+	return i, errUnexpectedType
+}
+
+func stepUntil(scanner *scanner, input string, i int, until int) (int, error) {
+	for ; i < len(input); i++ {
+		switch op := scanner.step(scanner, input[i]); op {
+		case scanError:
+			return i + 1, scanner.err
+		case scanSkipSpace, scanContinue:
+			continue
+		case until:
+			return i + 1, nil
+		}
+	}
+	return i, errUnexpectedType
+}
+
+func stepUntilEndObjectString(scanner *scanner, input string, i int) (j int, delim string, err error) {
+	for ; i < len(input); i++ {
+		op := scanner.step(scanner, input[i])
+		fmt.Println(op)
+		switch op {
+		case scanError:
+			return i + 1, "", scanner.err
+		case scanSkipSpace, scanContinue:
+			continue
+		case scanEndObject:
+			return i + 1, "", nil
+		case scanObjectValue:
+			return i + 1, ",", nil
+		}
+	}
+	return i, "", errUnexpectedType
+}
+
+func scanString(scanner *scanner, input string, i int) (value string, from, j int, err error) {
+	i, err = stepTo(scanner, input, i, scanBeginLiteral)
+	if err != nil {
+		return "", 0, i, err
+	}
+	from = i - 1
+	if input[from] != '"' {
+		return "", from, i + 1, errUnexpectedType
+	}
+	for ; i < len(input); i++ {
+		switch scanner.step(scanner, input[i]) {
+		case scanError:
+			return "", from, i + 1, scanner.err
+		case scanContinue:
+			if input[i] == '"' && input[i-1] != '\\' {
+				to := i + 1
+				return input[from:to], from, to, nil
+			}
+		default:
+			return "", from, i + 1, errUnexpectedType
+		}
+	}
+	return "", from, i, errUnexpectedEndOfString
+}
+
+// unquote converts a quoted JSON string literal into a Go string.
+// The JSON quoting rules are different from Go's, so strconv.Unquote cannot
+// be used.
+func unquote(s string) (t string, err error) {
+	err = json.Unmarshal([]byte(s), &t)
+	return t, err
+}
+
+// quote converts a Go string into a quoted JSON string literal.
+// The rules are different from Go's string quoting, so strconv.Unquote cannot
+// be used.
+func quote(s string) (t string, err error) {
+	buf, err := json.Marshal(s)
+	return string(buf), err
 }
--- pkg/obfuscate/appsec_test.go
+++ pkg/obfuscate/appsec_test.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -238,6 +239,451 @@ func TestObfuscateAppSec(t *testing.T) {
 				err = json.Unmarshal([]byte(tc.expected), &expected)
 				require.NoError(t, err)
 				require.Equal(t, expected, actual)
+			}
+		})
+	}
+}
+
+func TestX(t *testing.T) {
+	//o := appsecEventsObfuscator{}
+	//o.obfuscateAppSec(`{"toto":null,"triggers":[{"rule_matches":[{"parameters":[{"key_path":[0,1,"k1",2,"k3"],"highlight":["highlighted SENSITIVE value 1","highlighted SENSITIVE value 2","highlighted SENSITIVE value 3"],"value":"the entire SENSITIVE value"}]}]}]}`)
+}
+
+func TestObfuscateAppSecParameterValue(t *testing.T) {
+	i := []struct {
+		name                          string
+		input                         string
+		expectedSyntaxError           bool
+		expectedUnexpectedTypeError   bool
+		expectOutput                  string
+		expectedUnexpectedEndOfString bool
+	}{
+		{
+			name:         "one-sensitive-value",
+			input:        `"i am a SENSITIVE_VALUE value"`,
+			expectOutput: `"i am a ? value"`,
+		},
+		{
+			name:         "many-sensitive-values",
+			input:        `"SENSITIVE_VALUE i am a SENSITIVE_VALUE value SENSITIVE_VALUE"`,
+			expectOutput: `"? i am a ? value ?"`,
+		},
+		{
+			name:         "many-sensitive-values",
+			input:        `"      SENSITIVE_VALUE i am a      SENSITIVE_VALUE value      SENSITIVE_VALUE     "`,
+			expectOutput: `"      ? i am a      ? value      ?     "`,
+		},
+		{
+			name:         "no-sensitive-values",
+			input:        `"i am just a value"`,
+			expectOutput: `"i am just a value"`,
+		},
+		{
+			name:         "empty-json-string",
+			input:        `""`,
+			expectOutput: `""`,
+		},
+		{
+			name:                          "unterminated-json-string",
+			input:                         `"i am a SENSITIVE_VALUE value`,
+			expectOutput:                  `"i am a SENSITIVE_VALUE value`,
+			expectedUnexpectedEndOfString: true,
+		},
+		{
+			name:                        "empty-string",
+			input:                       ``,
+			expectOutput:                ``,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "null",
+			input:                       `null`,
+			expectOutput:                `null`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "object",
+			input:                       `{"k":"v"}`,
+			expectOutput:                `{"k":"v"}`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "array",
+			input:                       `[1,2,"three"]`,
+			expectOutput:                `[1,2,"three"]`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "float",
+			input:                       `1.5`,
+			expectOutput:                `1.5`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                "syntax-error",
+			input:               `"i am a SENSITIVE_VALUE \ `,
+			expectOutput:        `"i am a SENSITIVE_VALUE \ `,
+			expectedSyntaxError: true,
+		},
+	}
+	for _, tc := range i {
+		t.Run(tc.name, func(t *testing.T) {
+			for _, hasSensitiveKey := range []bool{true, false} {
+				var name string
+				if hasSensitiveKey {
+					name = "with-sensitive-key"
+				} else {
+					name = "without-sensitive-key"
+				}
+				t.Run(name, func(t *testing.T) {
+					o := appsecEventsObfuscator{
+						keyRE:   regexp.MustCompile("SENSITIVE_KEY"),
+						valueRE: regexp.MustCompile("SENSITIVE_VALUE"),
+					}
+					var diff Diff
+					scanner := &scanner{}
+					scanner.reset()
+					_, err := o.obfuscateAppSecParameterValue(scanner, tc.input, 0, &diff, hasSensitiveKey)
+					output := diff.Apply(tc.input)
+					if err != nil {
+						if tc.expectedSyntaxError {
+							require.Equal(t, scanner.err, err)
+						} else if tc.expectedUnexpectedTypeError {
+							require.Equal(t, errUnexpectedType, err)
+						} else if tc.expectedUnexpectedEndOfString {
+							require.Equal(t, errUnexpectedEndOfString, err)
+						} else {
+							require.NoError(t, err)
+						}
+						require.Empty(t, diff)
+						require.Equal(t, tc.expectOutput, output)
+					} else {
+						output := diff.Apply(tc.input)
+						if hasSensitiveKey {
+							require.Equal(t, `"?"`, output)
+						} else {
+							require.Equal(t, tc.expectOutput, output)
+						}
+					}
+				})
+			}
+		})
+	}
+}
+
+func TestObfuscateAppSecParameterHighlight(t *testing.T) {
+	i := []struct {
+		name                           string
+		input                          string
+		expectedSyntaxError            bool
+		expectedUnexpectedTypeError    bool
+		expectedOutput                 string
+		expectedOutputWithSensitiveKey string
+		expectedUnexpectedEndOfString  bool
+	}{
+		{
+			name:                           "one-sensitive-value",
+			input:                          `["i am a SENSITIVE_VALUE value"]`,
+			expectedOutput:                 `["i am a ? value"]`,
+			expectedOutputWithSensitiveKey: `["?"]`,
+		},
+		{
+			name:                           "many-sensitive-values",
+			input:                          `["SENSITIVE_VALUE i am a SENSITIVE_VALUE value SENSITIVE_VALUE"]`,
+			expectedOutput:                 `["? i am a ? value ?"]`,
+			expectedOutputWithSensitiveKey: `["?"]`,
+		},
+		{
+			name:                           "many-sensitive-values",
+			input:                          `["      SENSITIVE_VALUE i am a      SENSITIVE_VALUE value      SENSITIVE_VALUE     "]`,
+			expectedOutput:                 `["      ? i am a      ? value      ?     "]`,
+			expectedOutputWithSensitiveKey: `["?"]`,
+		},
+		{
+			name:                           "no-sensitive-values",
+			input:                          `["i am just a value"]`,
+			expectedOutput:                 `["i am just a value"]`,
+			expectedOutputWithSensitiveKey: `["?"]`,
+		},
+		{
+			name:                           "empty-array",
+			input:                          `[]`,
+			expectedOutput:                 `[]`,
+			expectedOutputWithSensitiveKey: `[]`,
+		},
+		{
+			name:                           "empty-json-string",
+			input:                          `[""]`,
+			expectedOutput:                 `[""]`,
+			expectedOutputWithSensitiveKey: `["?"]`,
+		},
+		{
+			name:                          "unterminated-json-string",
+			input:                         `["i am a SENSITIVE_VALUE value`,
+			expectedOutput:                `["i am a SENSITIVE_VALUE value`,
+			expectedUnexpectedEndOfString: true,
+		},
+		{
+			name:                        "empty-string",
+			input:                       ``,
+			expectedOutput:              ``,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "null",
+			input:                       `null`,
+			expectedOutput:              `null`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "object",
+			input:                       `{}`,
+			expectedOutput:              `{}`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "float",
+			input:                       `1.5`,
+			expectedOutput:              `1.5`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                "syntax-error",
+			input:               `["i am a SENSITIVE_VALUE \ `,
+			expectedOutput:      `["i am a SENSITIVE_VALUE \ `,
+			expectedSyntaxError: true,
+		},
+	}
+	for _, tc := range i {
+		t.Run(tc.name, func(t *testing.T) {
+			for _, hasSensitiveKey := range []bool{true, false} {
+				var name string
+				if hasSensitiveKey {
+					name = "with-sensitive-key"
+				} else {
+					name = "without-sensitive-key"
+				}
+				t.Run(name, func(t *testing.T) {
+					o := appsecEventsObfuscator{
+						keyRE:   regexp.MustCompile("SENSITIVE_KEY"),
+						valueRE: regexp.MustCompile("SENSITIVE_VALUE"),
+					}
+					var diff Diff
+					scanner := &scanner{}
+					scanner.reset()
+					_, err := o.obfuscateAppSecParameterHighlight(scanner, tc.input, 0, &diff, hasSensitiveKey)
+					output := diff.Apply(tc.input)
+					if err != nil {
+						if tc.expectedSyntaxError {
+							require.Equal(t, scanner.err, err)
+						} else if tc.expectedUnexpectedTypeError {
+							require.Equal(t, errUnexpectedType, err)
+						} else if tc.expectedUnexpectedEndOfString {
+							require.Equal(t, errUnexpectedEndOfString, err)
+						} else {
+							require.NoError(t, err)
+						}
+						require.Empty(t, diff)
+						require.Equal(t, tc.expectedOutput, output)
+					} else {
+						output := diff.Apply(tc.input)
+						if hasSensitiveKey {
+							require.Equal(t, tc.expectedOutputWithSensitiveKey, output)
+						} else {
+							require.Equal(t, tc.expectedOutput, output)
+						}
+					}
+				})
+			}
+		})
+	}
+}
+
+func TestObfuscateAppSecParameterKeyPath(t *testing.T) {
+	for _, tc := range []struct {
+		name                        string
+		input                       string
+		expectedSyntaxError         bool
+		expectedUnexpectedTypeError bool
+		expectedSensitiveKey        bool
+	}{
+		{
+			name:                 "flat",
+			input:                `[]`,
+			expectedSensitiveKey: false,
+		},
+		{
+			name:                 "flat",
+			input:                `[1,2,3,"four","SENSITIVE_KEY",5]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "flat-first",
+			input:                `[    "SENSITIVE_KEY"   , 1,2,3,"four" , 5]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "flat-middle",
+			input:                `[    "SENSITIVE_KEY"   ]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "flat-last",
+			input:                `[ 1,2,3,"four" , 5   ,      "SENSITIVE_KEY"   ]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "flat",
+			input:                `[1,2,3,"four",5]`,
+			expectedSensitiveKey: false,
+		},
+		{
+			name:                 "sub-array",
+			input:                `[1,2,3,"four","SENSITIVE_KEY",5, [ "SENSITIVE_KEY" ], 6]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "sub-array",
+			input:                `[1,2,3,"four",5, [ "SENSITIVE_KEY" ], 6]`,
+			expectedSensitiveKey: false,
+		},
+		{
+			name:                 "sub-array",
+			input:                `[1,2,3,"four",5, [[[]]], "SENSITIVE_KEY", 6]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "sub-object",
+			input:                `[1,2,3,"four",5, { "a": "b" }, 6]`,
+			expectedSensitiveKey: false,
+		},
+		{
+			name:                 "mixed",
+			input:                `[1,2,3,"four",5, { "key_path": [ "SENSITIVE_KEY" ] }, 6]`,
+			expectedSensitiveKey: false,
+		},
+		{
+			name:                 "mixed",
+			input:                `[1,2.2,3,"four",5, [ { "key_path": [ "SENSITIVE_KEY" ] } ], [{},[{},[{},[{},[{},[{}]]]]]], "SENSITIVE_KEY", 6]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "mixed",
+			input:                `["SENSITIVE_KEY", 1,2,3,"four",5, [ { "key_path": [ "SENSITIVE_KEY" ] } ], [{},[{},[{},[{},[{},[{}]]]]]], "SENSITIVE_KEY", 6]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                 "mixed",
+			input:                `[ 1,2,3,"four",5, [ { "key_path": [ "SENSITIVE_KEY" ] } ], [{},[{},[{},[{},[{},[{}]]]]]], "SENSITIVE_KEY", 6]`,
+			expectedSensitiveKey: true,
+		},
+		{
+			name:                "syntax-error",
+			input:               `[ 1,2,3,"four",5, [ { "key_path": [ "SENSITIVE_KEY" ] } ], [{},[{},[{},[{},[{},[{}]]]]]], "SENSITIVE_KEY" 6]`,
+			expectedSyntaxError: true,
+		},
+		{
+			name:                "syntax-error",
+			input:               `[ 1,2,3,"four",5, [ { [ "SENSITIVE_KEY" ] } ], [{},[{},[{},[{},[{},[{}]]]]]], "SENSITIVE_KEY", 6]`,
+			expectedSyntaxError: true,
+		},
+		{
+			name:                        "null",
+			input:                       `null`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "object",
+			input:                       `{}`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                        "unterminated",
+			input:                       `[ "SENSITIVE_KEY"`,
+			expectedUnexpectedTypeError: true,
+		},
+		{
+			name:                "syntax-error",
+			input:               `[ "SENSITIVE_KEY"" ]`,
+			expectedSyntaxError: true,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			o := appsecEventsObfuscator{
+				keyRE:   regexp.MustCompile("SENSITIVE_KEY"),
+				valueRE: regexp.MustCompile("SENSITIVE_VALUE"),
+			}
+			scanner := &scanner{}
+			scanner.reset()
+			hasSensitiveKey, i, err := o.obfuscateAppSecParameterKeyPath(scanner, tc.input, 0)
+			if tc.expectedSyntaxError {
+				require.Equal(t, scanner.err, err)
+			} else if tc.expectedUnexpectedTypeError {
+				require.Equal(t, errUnexpectedType, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, len(tc.input), i)
+			}
+			require.Equal(t, tc.expectedSensitiveKey, hasSensitiveKey)
+		})
+	}
+}
+
+func TestWalkObject(t *testing.T) {
+	for _, tc := range []struct {
+		name                        string
+		input                       string
+		expectedSeen                map[string]string
+		expectedSyntaxError         bool
+		expectedUnexpectedTypeError bool
+	}{
+		{
+			name:         "flat",
+			input:        `{}`,
+			expectedSeen: map[string]string{},
+		},
+		{
+			name:         "flat",
+			input:        `{   "key"      :    "  value  "   }`,
+			expectedSeen: map[string]string{`"key"`: `    "  value  "   `},
+		},
+		{
+			name:         "flat",
+			input:        `{   "key 1"      :    "  value 1  "  ,    "key 2"      :    "  value 2  "  ,  "key 3"      :    "  value 3  "   }`,
+			expectedSeen: map[string]string{`"key 1"`: `    "  value 1  "  `, `"key 2"`: `    "  value 2  "  `, `"key 3"`: `    "  value 3  "   `},
+		},
+		{
+			name:         "flat",
+			input:        `{"key":"  value  "}`,
+			expectedSeen: map[string]string{`"key"`: `"  value  "`},
+		},
+		{
+			name:         "flat",
+			input:        `{"key":["  value  "]}`,
+			expectedSeen: map[string]string{`"key"`: `["  value  "]`},
+		},
+		{
+			name:         "flat",
+			input:        `{"key":      [      "  value  "   ]      }`,
+			expectedSeen: map[string]string{`"key"`: `      [      "  value  "   ]      `},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			scanner := &scanner{}
+			scanner.reset()
+			seen := map[string]string{}
+			i, err := walkObject(scanner, tc.input, 0, func(key, value string) {
+				assert.NotContains(t, seen, key)
+				seen[key] = value
+			})
+			if tc.expectedSyntaxError {
+				require.Equal(t, scanner.err, err)
+			} else if tc.expectedUnexpectedTypeError {
+				require.Equal(t, errUnexpectedType, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, len(tc.input), i)
+				require.Equal(t, tc.expectedSeen, seen)
 			}
 		})
 	}
