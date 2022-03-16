40d38d1cb08f0955163748730dac9526888c5d67
Paul Cacheux <paul.cacheux@datadoghq.com>
2022-03-16 17:43:49 +0100

    Fix python linting issues

--- tasks/security_agent.py
+++ tasks/security_agent.py
@@ -532,9 +532,13 @@ def cws_go_generate(ctx):
         ctx.run("go generate ./...")
     ctx.run("go generate ./pkg/security/...")
 
+
 @task
 def generate_btfhub_constants(ctx, archive_path):
     output_path = "./pkg/security/probe/constantfetch/btfhub/constants.json"
     # we run the tool with `pty=True` because pahole, the underlying btf parsing tool,
     # is a bit sensible to terminal configuration
-    ctx.run(f"go run ./pkg/security/probe/constantfetch/btfhub/ -archive-root {archive_path} -output {output_path}", pty=True)
+    ctx.run(
+        f"go run ./pkg/security/probe/constantfetch/btfhub/ -archive-root {archive_path} -output {output_path}",
+        pty=True,
+    )
